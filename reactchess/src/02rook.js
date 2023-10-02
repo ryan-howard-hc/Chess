c5f3108755a8c934c14c786030cd2a1235776f91
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-10-02 16:56:13 -0400

    rook moves, but logic is whack

--- reactchess/src/02rook.js
+++ reactchess/src/02rook.js
@@ -4,14 +4,12 @@ const Rook = ({ color, fromRow, fromCol, boardState, onMove }) => {
   const calculateValidMoves = () => {
     const validMoves = [];
 
-    // Check valid moves in the horizontal direction
     for (let col = 0; col < 8; col++) {
       if (col !== fromCol && boardState[fromRow][col] === '') {
         validMoves.push({ toRow: fromRow, toCol: col });
       }
     }
 
-    // Check valid moves in the vertical direction
     for (let row = 0; row < 8; row++) {
       if (row !== fromRow && boardState[row][fromCol] === '') {
         validMoves.push({ toRow: row, toCol: fromCol });
@@ -23,9 +21,8 @@ const Rook = ({ color, fromRow, fromCol, boardState, onMove }) => {
 
   const handleMove = (toRow, toCol) => {
     const validMoves = calculateValidMoves();
-
-    // Check if the move is valid
-    if (validMoves.some(move => move.toRow === toRow && move.toCol === toCol)) {
+  
+    if (validMoves.length > 0 && validMoves.some(move => move.toRow === toRow && move.toCol === toCol)) {
       const newBoardState = [...boardState];
       newBoardState[toRow][toCol] = newBoardState[fromRow][fromCol];
       newBoardState[fromRow][fromCol] = '';
@@ -39,7 +36,7 @@ const Rook = ({ color, fromRow, fromCol, boardState, onMove }) => {
 
   return (
     <div className={`piece rook ${color}`} onClick={() => handleMove(validMoves[0].toRow, validMoves[0].toCol)}>
-      R {/* Display 'R' or any other symbol as the Rook */}
+      R 
     </div>
   );
 };
