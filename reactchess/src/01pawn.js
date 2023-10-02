96e525ff38ae86389db4e3e4da2a6ccd7edd04d9
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-10-02 17:12:14 -0400

    moves piece to any square, no turns, etc

--- reactchess/src/01pawn.js
+++ reactchess/src/01pawn.js
@@ -1,44 +1,44 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// const Pawn = ({ piece, position, movePawn }) => {
-//   const [isSelected, setIsSelected] = useState(false);
+const Pawn = ({ piece, position, movePawn }) => {
+  const [isSelected, setIsSelected] = useState(false);
 
-//   const handleClick = () => {
-//     if (isSelected) {
-//       setIsSelected(false);
-//     } else {
-//       setIsSelected(true);
-//     }
-//   };
+  const handleClick = () => {
+    if (isSelected) {
+      setIsSelected(false);
+    } else {
+      setIsSelected(true);
+    }
+  };
 
-//   const handleMove = (toRow, toCol) => {
-//     movePawn(position[0], position[1], toRow, toCol);
-//     setIsSelected(false);
-//   };
+  const handleMove = (toRow, toCol) => {
+    movePawn(position[0], position[1], toRow, toCol);
+    setIsSelected(false);
+  };
 
-//   return (
-//     <div
-//       className={`pawn ${isSelected ? 'selected' : ''}`}
-//       onClick={handleClick}
-//     >
-//       {piece}
-//       {isSelected && (
-//         <div className="valid-moves">
-//           <button onClick={() => handleMove(position[0] - 1, position[1])}>
-//             Move Up
-//           </button>
+  return (
+    <div
+      className={`pawn ${isSelected ? 'selected' : ''}`}
+      onClick={handleClick}
+    >
+      {piece}
+      {isSelected && (
+        <div className="valid-moves">
+          <button onClick={() => handleMove(position[0] - 1, position[1])}>
+            Move Up
+          </button>
 
 
 
-//           {/*  */}
+          {/*  */}
 
 
 
           
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
+        </div>
+      )}
+    </div>
+  );
+};
 
-// export default Pawn;
+export default Pawn;
--- reactchess/src/0gameboard.js
+++ reactchess/src/0gameboard.js
@@ -2,57 +2,65 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/css/chessboard.css';
 import Pawn from './01pawn';
-import Rook from './02rook';
+
 const Chessboard = () => {
-    const initialBoardState = [
-        ['R', 'P', '', '', '', '', 'P', 'R'],
-        ['N', 'P', '', '', '', '', 'P', 'N'],
-        ['B', 'P', '', '', '', '', 'P', 'B'],
-        ['Q', 'P', '', '', '', '', 'P', 'Q'],
-        ['K', 'P', '', '', '', '', 'P', 'K'],
-        ['B', 'P', '', '', '', '', 'P', 'B'],
-        ['N', 'P', '', '', '', '', 'P', 'N'],
-        ['R', 'P', '', '', '', '', 'P', 'R'],
-      ];
-    
-      const [boardState, setBoardState] = useState(initialBoardState);
-  
-  
-    // one const to movepieces, will update accordingly
-    const movePiece = (fromRow, fromCol, toRow, toCol) => {
-      // Check if there is a piece at the source position
-      if (boardState[fromRow][fromCol] === '') {
-        console.log('No piece to move!');
-        return;
-      }
-  
-      if (toRow < 0 || toRow >= 8 || toCol < 0 || toCol >= 8) {
-        console.log('Invalid destination!');
-        return; 
-      }
-  
-      //  proceed with move
-      const newBoardState = [...boardState];
-      newBoardState[toRow][toCol] = newBoardState[fromRow][fromCol];
-      newBoardState[fromRow][fromCol] = '';
-      setBoardState(newBoardState);
-    };
-  
-    return (
-        <div className="chessboard">
-          {boardState.map((row, rowIndex) => (
-            row.map((piece, colIndex) => (
-                <div
-                key={`${rowIndex}-${colIndex}`}
-                className={`square ${((rowIndex + colIndex) % 2 === 0) ? 'white' : 'black'}`}
-                onClick={() => movePiece(rowIndex, colIndex)}
-              >
-            {piece === 'R' && <Rook color="white" fromRow={rowIndex} fromCol={colIndex} boardState={boardState} onMove={setBoardState} />}
-              </div>
-            ))
-          ))}
+  const initialBoardState = [
+    ['R', 'P', '', '', '', '', 'p', 'r'],
+    ['N', 'P', '', '', '', '', 'p', 'n'],
+    ['B', 'P', '', '', '', '', 'p', 'b'],
+    ['Q', 'P', '', '', '', '', 'p', 'q'],
+    ['K', 'P', '', '', '', '', 'p', 'k'],
+    ['B', 'P', '', '', '', '', 'p', 'b'],
+    ['N', 'P', '', '', '', '', 'p', 'n'],
+    ['R', 'P', '', '', '', '', 'p', 'r'],
+  ];
+  const [boardState, setBoardState] = useState(initialBoardState);
+
+  const movePiece = (fromRow, fromCol, toRow, toCol) => {
+    const newBoardState = [...boardState];
+    newBoardState[toRow][toCol] = newBoardState[fromRow][fromCol];
+    newBoardState[fromRow][fromCol] = '';
+    setBoardState(newBoardState);
+  };
+
+  const board = [];
+
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      const squareColor = (row + col) % 2 === 0 ? 'white' : 'black';
+      const squareId = `${String.fromCharCode(97 + col)}${8 - row}`;
+      const piece = boardState[row][col];
+
+      board.push(
+        <div
+          key={squareId}
+          className={`square ${squareColor}`}
+          onClick={() => {
+            if (piece !== '') {
+              // handle piece movement
+              // move any piece to an empty square
+              const targetPiece = prompt('Enter the target square:');
+              if (targetPiece) {
+                const targetCol = targetPiece.charCodeAt(0) - 97;
+                const targetRow = 8 - parseInt(targetPiece[1], 10);
+                movePiece(row, col, targetRow, targetCol);
+              }
+            }
+          }}
+        >
+          {piece && (
+            <Pawn
+              piece={piece}
+              position={[row, col]}
+              movePiece={movePiece}
+            />
+          )}
         </div>
       );
-    };
-    
-    export default Chessboard;
+    }
+  }
+
+  return <div className="chessboard">{board}</div>;
+};
+
+export default Chessboard;
