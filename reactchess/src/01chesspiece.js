e0c25af167f1ee32f79aa58c1812ccd33d0de741
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-10-08 13:44:18 -0400

    chesspiece v3

--- reactchess/src/01chesspiece.js
+++ reactchess/src/01chesspiece.js
@@ -10,8 +10,21 @@ import frodo from '../src/css/icons/frodo.png';
 import bat from '../src/css/icons/bat.png';
 import knight from '../src/css/icons/knight.png';
 import queen from '../src/css/icons/queen.png';
-
 import demon from '../src/css/icons/demon.png';
+
+import devil from '../src/css/icons/devil.png';
+import ghost from '../src/css/icons/ghost.png';
+import grail from '../src/css/icons/holy-grail.png';
+import water from '../src/css/icons/holy-water.png';
+import pope from '../src/css/icons/pope.png';
+import angel from '../src/css/icons/power.png';
+import soldier from '../src/css/icons/soldier.png';
+import zombie from '../src/css/icons/zombie.png';
+import tree from '../src/css/icons/tree.png';
+import witch from '../src/css/icons/witch-hat.png';
+import peace from '../src/css/icons/peace.png';
+
+
 
 
 const ChessPiece = ({ piece, design }) => {
@@ -44,6 +57,21 @@ const ChessPiece = ({ piece, design }) => {
       'r': <img src={wolf} alt="Orc" />,
       'q': <img src={demon} alt="Orc" />,
       'k': <img src={axe} alt="Orc" />,
+      ' ': null,
+    },
+    altAlt: {
+      'P':<img src={soldier} alt="Orc" />,
+      'N': <img src={water} alt="Orc" />,
+      'B': <img src={pope} alt="Orc" />,
+      'R': <img src={peace} alt="Orc" />,
+      'Q': <img src={angel} alt="Orc" />,
+      'K': <img src={grail} alt="Orc" />,
+      'p': <img src={skeleton} alt="Orc" />,
+      'n': <img src={zombie} alt="Orc" />,
+      'b': <img src={ghost} alt="Orc" />,
+      'r': <img src={tree} alt="Orc" />,
+      'q': <img src={devil} alt="Orc" />,
+      'k': <img src={witch} alt="Orc" />,
       ' ': null,
     },
   };
